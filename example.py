97fb03e4448220936b3b9cc0dca3701d0f36cb06
Mike Plis <Michael.plis01@gmail.com>
2013-02-13 14:29:56 -0500

    obtain json of graph in example; updated values in sample-system

--- example.py
+++ example.py
@@ -1,7 +1,9 @@
 import networkx as nx
+from networkx.readwrite import json_graph
 import itertools
 import numpy
 import matplotlib.pyplot as plt
+import json
 
 G = nx.DiGraph()
 banks = ['Bank 1', 'Bank 2', 'Bank 3', 'Bank 4', 'Bank 5']
@@ -9,6 +11,6 @@ G.add_nodes_from(banks, capital = 0, shock = 0)
 bank_edges = itertools.permutations(banks, 2)
 G.add_edges_from(bank_edges, payment = 1.0)
 
-"""pos = nx.spring_layout(G)
-nx.draw(G, pos, node_color='#A0CBE2')
-plt.show()"""
+data = json_graph.node_link_data(G)
+s = json.dumps(data)
+
